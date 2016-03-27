2e2c9f6b2cad18d3a93dcc19f82b854cc3ffb8af
William Rebeque <williamrebeque@gmail.com>
2016-03-27 18:54:33 -0300

    ---

--- www/js/menu.js
+++ www/js/menu.js
@@ -1,16 +1,21 @@
-/*
+
 $(document).ready(function(){
-    var x = 1;
+    var x = 0;
     var array = [1,2,3,4,5,6,7,8,9,10];
-    for(y = 0; y < 10; y++){
-        var texto = "<div>"+array[y]+"</div>";
-        $("#teste1").append(texto); 
+    var div = "<div style='width: 100%; height: 100%;'>";
+    div += "<table style='width: 100%; height: 100%;'><tr>";
+    for(y = 0; y < 8; y++){
+        div += "<td><div>"+array[y]+"</div></td>";
+        if(y == 3){
+            div += "</tr><tr>";
+        }
     }
     
-
+    div += "</table></div>";
+    $("#pacientes").append(texto);
 });
 
-*/
+
 function sair(){
     $(location).attr('href', '../index.html');
 }
