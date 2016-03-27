fb3acf28983e7b09f6c8dd35333d23245e79efa7
William Rebeque <williamrebeque@gmail.com>
2016-03-27 17:49:37 -0300

    menu com setas

--- www/js/menu.js
+++ www/js/menu.js
@@ -1,3 +1,17 @@
+/*
+$(document).ready(function(){
+    var x = 1;
+    var array = [1,2,3,4,5,6,7,8,9,10];
+    for(y = 0; y < 10; y++){
+        var texto = "<div>"+array[y]+"</div>";
+        $("#teste1").append(texto); 
+    }
+    
+
+});
+
+*/
 function sair(){
     $(location).attr('href', '../index.html');
 }
+
